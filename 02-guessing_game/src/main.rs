b5b566935340daa85452057bd214e94546106c62
Hugo Orlando <hugo.orlando2050@gmail.com>
2024-01-08 12:32:29 -0300

    :rocket: add new features

--- 02-guessing_game/src/main.rs
+++ 02-guessing_game/src/main.rs
@@ -1,16 +1,42 @@
-use std::io; // biblioteca estandar de entrada/salida io
-             // std::io te proporciona una serie de características útiles, incluyendo la capacidad de aceptar la entrada del usuario
+use rand::Rng;
+use std::cmp::Ordering;
+// biblioteca estandar de entrada/salida io
+// std::io te proporciona una serie de características útiles, incluyendo la capacidad de aceptar la entrada del usuario
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    // función rand::thread_rng que nos da el generador de números aleatorios particular
+    // El método gen_range toma una expresión de rango como argumento y genera un número aleatorio en el rango
+    // 1..=100 para solicitar un número entre 1 y 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    // declaración let para crear la variable
+    // variable mutable guess
+    // String::new() función que devuelve una nueva instancia de un String
+    // La sintaxis :: en la línea ::new indica que new es una función asociada del tipo String
+    let mut guess = String::new(); // ha creado una variable mutable que está actualmente enlazada a una nueva instancia vacía de un String
 
+    // función stdin del módulo io, que nos permitirá manejar la entrada del usuario
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    // Comparando la adivinanza con el Número Secreto
+
+    // Shadowing de guess
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    // El tipo Ordering es otro enum y tiene las variantes Less, Greater y Equal. Estos son los tres resultados posibles cuando compara dos valores
+    // El método cmp compara dos valores y se puede llamar en cualquier cosa que se pueda comparar
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
