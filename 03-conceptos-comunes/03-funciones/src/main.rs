b500a054da3b72fced150cf650fb3d214dea866c
Hugo Orlando <hugo.orlando2050@gmail.com>
2024-01-08 15:42:33 -0300

    :tada: add new features

--- 03-conceptos-comunes/03-funciones/src/main.rs
+++ 03-conceptos-comunes/03-funciones/src/main.rs
@@ -4,6 +4,12 @@ fn main() {
     another_function();
     another_function_two(5);
     print_labeled_measurement(5, 'h');
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let y = plus_one(7);
+    println!("The value of x is: {y}");
 }
 
 fn another_function() {
@@ -16,4 +22,13 @@ fn another_function_two(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+// Funciones con valores de retorno
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
