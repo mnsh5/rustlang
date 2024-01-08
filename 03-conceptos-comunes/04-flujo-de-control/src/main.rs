42ac6dbadb421047dcf2f899138d8c06cea299b5
Hugo Orlando <hugo.orlando2050@gmail.com>
2024-01-08 15:59:25 -0300

    🐆 add flujo de control

--- 03-conceptos-comunes/04-flujo-de-control/src/main.rs
+++ 03-conceptos-comunes/04-flujo-de-control/src/main.rs
@@ -6,4 +6,73 @@ fn main() {
     } else {
         println!("condition was false");
     }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    // Devolviendo valores de los bucles
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    // Etiquetas de bucle para distinguir entre varios bucles
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // Bucles condicionales con while
+    let mut num = 3;
+    while num != 0 {
+        println!("{num}!");
+        num -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    // Bucle a través de una colección con for
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for num in (1..4).rev() {
+        println!("{num}!");
+    }
+    println!("LIFTOFF!!!");
 }
