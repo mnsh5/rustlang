35b10844e7cc14e6d9343790a84b2463b7af6849
mnsh4 <hugo.orlando2050@gmail.com>
2024-01-14 17:38:31 -0300

    :tada: add new features

--- 06-enums-patthern_matching/02-flujo-match/src/main.rs
+++ 06-enums-patthern_matching/02-flujo-match/src/main.rs
@@ -2,24 +2,57 @@
 // Rust tiene una construcción de flujo de control extremadamente poderosa llamada match que te permite comparar un valor
 // contra una serie de patrones y luego ejecutar código basado en qué patrón coincide
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        // Coin::Penny => 1,
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
-fn main() {}
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
+}
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         // Coin::Penny => 1,
+//         Coin::Penny => {
+//             println!("Lucky penny!");
+//             1
+//         }
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
